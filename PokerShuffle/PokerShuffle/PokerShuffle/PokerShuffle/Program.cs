0fb61f1a3b21511100d7655384ead4cbec3121d4
wangj1ngyan <joannawjy0179@gmail.com>
2024-03-27 12:54:38 +0800

    Remove submodule and add as regular files

---
+++ PokerShuffle/PokerShuffle/PokerShuffle/PokerShuffle/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CardFramework;
+using PokerFramework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Game game = new Game();
+        game.Play();
+    }
+}
